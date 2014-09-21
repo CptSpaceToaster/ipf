87cb08fa54390df33ed83aa94e3a82381e307188
AetheralLabyrinth <philvo7@gmail.com>
2014-09-20 23:33:34 -0400

    Added timer0 and 2 pwm functionality

--- adc_setup.h
+++ adc_setup.h
@@ -17,21 +17,16 @@ void ADC_init(uint8_t channel, uint8_t trigger_source, bool left_aligned, bool a
 	uint8_t adc_prescaler;
 
 	// Error checking
-	if( channel > 8 || channel < 0 )
-	{
+	if( channel > 8 || channel < 0 ) {
 		printf("Error: The selected channel is outside the bounds of possible ADC channels 0 to 8.\n");
 		return;
 	}
-
 	if( adc_prescaler != 2 && adc_prescaler != 4 && adc_prescaler != 8 && adc_prescaler != 16 &&
-	adc_prescaler != 32 && adc_prescaler != 64 && adc_prescaler != 128 )
-	{
+	adc_prescaler != 32 && adc_prescaler != 64 && adc_prescaler != 128 ) {
 		printf("Error: The selected prescaler is not an acceptable value {2, 4, 8, 16, 32, 64, 128}\n");
 		return;
 	}
-
-	if( trigger_source > 7 || trigger_source < 0 )
-	{
+	if( trigger_source > 7 || trigger_source < 0 ) {
 		printf("Error: The selected trigger source is invalid. Value must be between 0 and 7 (0b000to 0b111)\n");
 		return;
 	}
@@ -41,32 +36,19 @@ void ADC_init(uint8_t channel, uint8_t trigger_source, bool left_aligned, bool a
 	ADCSRA = _BV(ADEN) | _BV(ADATE) | _BV(ADIE) * adc_interrupt_enable; // ADC enable, Autotrigger, Interrupt flag
 
 	// determine appropriate prescaler, F_CPU must be defined before hand!
-	if((F_CPU / 2) > 50000 && (F_CPU / 2) < 200000)
-	{
+	if((F_CPU / 2) > 50000 && (F_CPU / 2) < 200000) {
 		adc_prescaler = 2;
-	}
-	else if((F_CPU / 4) > 50000 && (F_CPU / 4) < 200000)
-	{
+	} else if((F_CPU / 4) > 50000 && (F_CPU / 4) < 200000) {
 		adc_prescaler = 4;
-	}
-	else if((F_CPU / 8) > 50000 && (F_CPU / 8) < 200000)
-	{
+	} else if((F_CPU / 8) > 50000 && (F_CPU / 8) < 200000) {
 		adc_prescaler = 8;
-	}
-	else if((F_CPU / 16) > 50000 && (F_CPU / 16) < 200000)
-	{
+	} else if((F_CPU / 16) > 50000 && (F_CPU / 16) < 200000) {
 		adc_prescaler = 16;
-	}
-		else if((F_CPU / 32) > 50000 && (F_CPU / 32) < 200000)
-	{
+	} else if((F_CPU / 32) > 50000 && (F_CPU / 32) < 200000) {
 		adc_prescaler = 32;
-	}
-	else if((F_CPU / 64) > 50000 && (F_CPU / 64) < 200000)
-	{
+	} else if((F_CPU / 64) > 50000 && (F_CPU / 64) < 200000) {
 		adc_prescaler = 64;
-	}
-	else
-	{
+	} else {
 		adc_prescaler = 128;
 	}
 
@@ -74,90 +56,90 @@ void ADC_init(uint8_t channel, uint8_t trigger_source, bool left_aligned, bool a
 	switch(channel)
 	{
 		case 0:
-		DDRC &= ~_BV(0);
-		break;
+			DDRC &= ~_BV(0);
+			break;
 		case 1:
-		DDRC &= ~_BV(1);
-		ADMUX |= _BV(MUX0);
-		break;
+			DDRC &= ~_BV(1);
+			ADMUX |= _BV(MUX0);
+			break;
 		case 2:
-		DDRC &= ~_BV(2);
-		ADMUX |= _BV(MUX1);
-		break;
+			DDRC &= ~_BV(2);
+			ADMUX |= _BV(MUX1);
+			break;
 		case 3:
-		DDRC &= ~_BV(3);
-		ADMUX |= _BV(MUX1) | _BV(MUX0);
-		break;
+			DDRC &= ~_BV(3);
+			ADMUX |= _BV(MUX1) | _BV(MUX0);
+			break;
 		case 4:
-		DDRC &= ~_BV(4);
-		ADMUX |= _BV(MUX2);
-		break;
+			DDRC &= ~_BV(4);
+			ADMUX |= _BV(MUX2);
+			break;
 		case 5:
-		DDRC &= ~_BV(5);
-		ADMUX |= _BV(MUX2) | _BV(MUX0);
-		break;
+			DDRC &= ~_BV(5);
+			ADMUX |= _BV(MUX2) | _BV(MUX0);
+			break;
 		case 6:
-		ADMUX |= _BV(MUX2) | _BV(MUX1);
-		break;
+			ADMUX |= _BV(MUX2) | _BV(MUX1);
+			break;
 		case 7:
-		ADMUX |= _BV(MUX2) | _BV(MUX1) | _BV(MUX0);
-		break;
+			ADMUX |= _BV(MUX2) | _BV(MUX1) | _BV(MUX0);
+			break;
 		case 8:
-		ADMUX |= _BV(MUX3);
-		break;
+			ADMUX |= _BV(MUX3);
+			break;
 	}
 	// Checks the prescaler setting and sets the proper bits in ADCSRA
 	switch(adc_prescaler)
 	{
 		case 2:
-		break;
+			break;
 		case 4:
-		ADCSRA |= _BV(ADPS1); // _BV(1)
-		break;
+			ADCSRA |= _BV(ADPS1); // _BV(1)
+			break;
 		case 8:
-		ADCSRA |= _BV(ADPS1) | _BV(ADPS0); //_BV(1)  | _BV(0)
-		break;
+			ADCSRA |= _BV(ADPS1) | _BV(ADPS0); //_BV(1)  | _BV(0)
+			break;
 		case 16:
-		ADCSRA |= _BV(ADPS2); // _BV(2)
-		break;
+			ADCSRA |= _BV(ADPS2); // _BV(2)
+			break;
 		case 32:
-		ADCSRA |= _BV(ADPS2) | _BV(ADPS0); // _BV(2) | _BV(0)
-		break;
+			ADCSRA |= _BV(ADPS2) | _BV(ADPS0); // _BV(2) | _BV(0)
+			break;
 		case 64:
-		ADCSRA |= _BV(ADPS2) | _BV(ADPS1); // _BV(2) | _BV(1)
-		break;
+			ADCSRA |= _BV(ADPS2) | _BV(ADPS1); // _BV(2) | _BV(1)
+			break;
 		case 128:
-		ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // _BV(2) | _BV(1) | _BV(0)
-		break;
+			ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // _BV(2) | _BV(1) | _BV(0)
+			break;
 	}
 
 	// Checks the trigger source setting and sets the proper bits in ADCSRA
 	switch(trigger_source)
 	{
 		case 0:
-		ADCSRA |= _BV(ADSC); // free running mode, start conversions
-		break;
+			ADCSRA |= _BV(ADSC); // free running mode, start conversions
+			break;
 		case 1:
-		ADCSRB |= _BV(ADTS0); // analog comparator
-		break;
+			ADCSRB |= _BV(ADTS0); // analog comparator
+			break;
 		case 2:
-		ADCSRB |= _BV(ADTS1); // external interrupt request 0
-		break;
+			ADCSRB |= _BV(ADTS1); // external interrupt request 0
+			break;
 		case 3:
-		ADCSRB |= _BV(ADTS1) | _BV(ADTS0); // timer/counter0 compare match A
-		break;
+			ADCSRB |= _BV(ADTS1) | _BV(ADTS0); // timer/counter0 compare match A
+			break;
 		case 4:
-		ADCSRB |= _BV(ADTS2); // timer/counter0 overflow
-		break;
+			ADCSRB |= _BV(ADTS2); // timer/counter0 overflow
+			break;
 		case 5:
-		ADCSRB |= _BV(ADTS2) | _BV(ADTS0); // timer/counter1 compare match B
-		break;
+			ADCSRB |= _BV(ADTS2) | _BV(ADTS0); // timer/counter1 compare match B
+			break;
 		case 6:
-		ADCSRB |= _BV(ADTS1) | _BV(ADTS2); // timer/counter overflow
-		break;
+			ADCSRB |= _BV(ADTS1) | _BV(ADTS2); // timer/counter overflow
+			break;
 		case 7:
-		ADCSRB |= _BV(ADTS2) | _BV(ADTS1) | _BV(ADTS0); // timer/counter 1 capture event
-		break;
+			ADCSRB |= _BV(ADTS2) | _BV(ADTS1) | _BV(ADTS0); // timer/counter 1 capture event
+			break;
 	}
 }
 
--- timer_setup.h
+++ timer_setup.h
@@ -1,4 +1,4 @@
-/* Author: CptSpaceToaster */
+/* Author: CptSpaceToaster and AetheralLabyrint*/
 /* YOU STILL NEED TO INVOKE SEI() YOURSELF! DON'T FORGET */
 
 #ifndef _IPF_TIMER_SETUP_
@@ -101,6 +101,66 @@ int timer2_ctc(double period, bool enable_compare_A) {
 	return 0;
 }
 
+/* Takes in cycles on, cycles in a period,  which pin is the output (in PortD), and whether or not you want to enable output compare's*/
+int timer0_pwm_prescaler_compare(uint8_t Ton, uint8_t period, uint8_t prescaler, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
+	if (output_pin >= 0 && output_pin <= 5 || output_pin > 6) {
+		printf("Error: Output pin must be 5, 6, or below 0\n");
+		return 1;
+	} else if (output_pin>0) {
+		DDRD |= _BV(output_pin); // Force output_pin to actually be an OUTPUT
+	}
+	TIMSK0 = _BV(OCIE0A)*enable_compare_A | _BV(OCIE0B)*enable_compare_B;
+	OCR0A = period >> 1; // We are counting up AND down, so we need to half the incoming cycle count to maintain accuracy
+	OCR0B = Ton >> 1;
+	// Allow Timer 0 to control PD5 or PD6, set timer to PWM mode with ceiling at OCR0A
+	TCCR0A = _BV(COM0A1)*(output_pin==5) | _BV(COM0B1)*(output_pin==6) | _BV(WGM00);
+	// Start the timer with the given prescaler, finish setting timer to PWM mode with ceiling at OCR0A
+	TCCR0B	= _BV(WGM02) | prescaler;
+	return 0;
+}
+
+/* Takes in Time-on, and period in seconds, and whether or not you want to enable output compare's*/
+int timer0_pwm_sec_compare(double Ton, double period, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
+	if (Ton < 0 || period < 0) {
+		printf("Error: Time was set to a value below 0\n");
+		return 1;
+	}
+	// Get cycle counts from input times
+	uint32_t cycles_on = Ton*(F_CPU);
+	uint32_t cycles_period = period*(F_CPU);
+	if (cycles_period > (uint32_t)1024 * 256) {
+		printf("Error: Value exceeded maximum timer0 threshold\n");
+		return 1;
+	}
+	if (Ton > period) {
+		printf("Error: Ton exceeded period\n");
+		return 1;
+	}
+	// Calculate prescaler
+	uint8_t prescaler=1;
+	while(cycles_period>256) {
+		cycles_on/=prescaler>2?4:8;
+		cycles_period/=prescaler>2?4:8;
+		prescaler++;
+	}
+	return timer0_pwm_prescaler_compare((uint8_t)cycles_on, (uint8_t)cycles_period, prescaler, output_pin, enable_compare_A, enable_compare_B);
+}
+
+/* A nice shortcut if you don't want to mess with interrupts */
+int timer0_pwm_sec(double Ton, double period, int8_t output_pin) {
+	return timer0_pwm_sec_compare(Ton, period, output_pin, false, false);
+}
+
+/* A nice shortcut if you don't want to mess with the prescaler */
+int timer0_pwm_compare(uint8_t Ton, uint8_t period, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
+	return timer0_pwm_prescaler_compare(Ton, period, 1, output_pin, enable_compare_A, enable_compare_B);
+}
+
+/* Another nice shortcut if you don't want to mess with interrupts and prescaler */
+int timer0_pwm(uint8_t Ton, uint8_t period, int8_t output_pin) {
+	return timer0_pwm_prescaler_compare(Ton, period, 1, output_pin, false, false);
+}
+
 /* Takes in cycles on, cycles in a period,  which pin is the output (in PortB), and whether or not you want to enable output compare's*/
 int timer1_pwm_prescaler_compare(uint16_t Ton, uint16_t period, uint8_t prescaler, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
 	if (output_pin == 0 || output_pin > 2) {
@@ -119,7 +179,7 @@ int timer1_pwm_prescaler_compare(uint16_t Ton, uint16_t period, uint8_t prescale
 	return 0;
 }
 
-/* Takes in Time-on, and period in seconds, and whether or not you want to enable output compare's*/
+/* Takes in Time-on, and period in seconds, and whether or not you want to enable output compares*/
 int timer1_pwm_sec_compare(double Ton, double period, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
 	if (Ton < 0 || period < 0) {
 		printf("Error: Time was set to a value below 0\n");
@@ -159,6 +219,76 @@ int timer1_pwm_compare(uint16_t Ton, uint16_t period, int8_t output_pin, bool en
 /* Another nice shortcut if you don't want to mess with interrupts and prescaler */
 int timer1_pwm(uint16_t Ton, uint16_t period, int8_t output_pin) {
 	return timer1_pwm_prescaler_compare(Ton, period, 1, output_pin, false, false);
+}
+
+/* Takes in cycles on, cycles in a period,  which pin is the output (3 for PB3 and 4 for PD3), and whether or not you want to enable output compare's*/
+int timer2_pwm_prescaler_compare(uint8_t Ton, uint8_t period, uint8_t prescaler, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
+	if (output_pin >= 0 && output_pin < 3 || output_pin > 4) {
+		printf("Error: Output pin must be 3, 4, or below 0\n");
+		return 1;
+	} else {
+		int8_t portd_output_pin = 0;
+		int8_t portb_output_pin = 0;
+		if (output_pin == 4) {
+			DDRD |= _BV(3); // Force output_pin to actually be an OUTPUT
+			portd_output_pin = 3;
+		}
+		if (output_pin == 3) {
+			DDRB |= _BV(3);
+			portb_output_pin = 3;
+		}
+	}
+
+	TIMSK2 = _BV(OCIE2A)*enable_compare_A | _BV(OCIE2B)*enable_compare_B;
+	OCR2A = period >> 1; // We are counting up AND down, so we need to half the incoming cycle count to maintain accuracy
+	OCR2B = Ton >> 1;
+	// Allow Timer 2 to control PD3 or PB3, set timer to PWM mode with ceiling at OCR2A
+	TCCR2A = _BV(COM2A1)*(portb_output_pin==3) | _BV(COM2B1)*(portd_output_pin==3) | _BV(WGM20);
+	// Start the timer with the given prescaler, finish setting timer to PWM mode with ceiling at OCR2A
+	TCCR2B	= _BV(WGM22) | prescaler;
+	return 0;
+}
+
+/* Takes in Time-on, and period in seconds, and whether or not you want to enable output compares*/
+int timer2_pwm_sec_compare(double Ton, double period, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
+	if (Ton < 0 || period < 0) {
+		printf("Error: Time was set to a value below 0\n");
+		return 1;
+	}
+	// Get cycle counts from input times
+	uint32_t cycles_on = Ton*(F_CPU);
+	uint32_t cycles_period = period*(F_CPU);
+	if (cycles_period > (uint32_t)1024 * 256) {
+		printf("Error: Value exceeded maximum timer2 threshold\n");
+		return 1;
+	}
+	if (Ton > period) {
+		printf("Error: Ton exceeded period\n");
+		return 1;
+	}
+	// Calculate prescaler
+	uint8_t prescaler=1;
+	while(cycles_period>256) {
+		cycles_on/=prescaler>2?4:8;
+		cycles_period/=prescaler>2?4:8;
+		prescaler++;
+	}
+	return timer2_pwm_prescaler_compare((uint8_t)cycles_on, (uint8_t)cycles_period, prescaler, output_pin, enable_compare_A, enable_compare_B);
+}
+
+/* A nice shortcut if you don't want to mess with interrupts */
+int timer2_pwm_sec(double Ton, double period, int8_t output_pin) {
+	return timer2_pwm_sec_compare(Ton, period, output_pin, false, false);
+}
+
+/* A nice shortcut if you don't want to mess with the prescaler */
+int timer2_pwm_compare(uint8_t Ton, uint8_t period, int8_t output_pin, bool enable_compare_A, bool enable_compare_B) {
+	return timer2_pwm_prescaler_compare(Ton, period, 1, output_pin, enable_compare_A, enable_compare_B);
+}
+
+/* Another nice shortcut if you don't want to mess with interrupts and prescaler */
+int timer2_pwm(uint8_t Ton, uint8_t period, int8_t output_pin) {
+	return timer2_pwm_prescaler_compare(Ton, period, 1, output_pin, false, false);
 }
 
 #endif //_IPF_TIMER_SETUP_
